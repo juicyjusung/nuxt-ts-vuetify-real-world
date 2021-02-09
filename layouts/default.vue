d2eaf5a84475e005c9ea8f1d2bc1e3bd3e923a08
juicyjusung <juicyjusung@gmail.com>
2021-02-10 01:24:11 +0900

    feat: add user menu button to layout header

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,11 +15,25 @@
         </nuxt-link>
       </div>
       <div v-else>
-        <juicy-btn
-          label="Log out"
-          icon="mdi-account-arrow-right"
-          @click="onClickLogout"
-        ></juicy-btn>
+        <juicy-btn label="new article" icon="mdi-pencil-plus" icon-pos="left"></juicy-btn>
+        <v-menu open-on-hover bottom offset-y>
+          <template #activator="{ on, attrs }">
+            <v-btn color="primary" v-bind="attrs" v-on="on">
+              {{ $accessor.userModule.user.username }}
+            </v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item v-for="(item, index) in items" :key="index" link @click="item.onClick">
+              <v-list-item-icon>
+                <v-icon>
+                  {{ item.icon }}
+                </v-icon>
+              </v-list-item-icon>
+              <v-list-item-title> {{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
       </div>
     </v-app-bar>
     <v-main>
@@ -40,6 +54,15 @@ import { notifySuccess } from '~/utils/notify';
   },
 })
 export default class DefaultLayout extends Vue {
+  items = [
+    { title: 'settings', icon: 'mdi-account-settings', onClick: this.onClickSettings },
+    { title: 'logout', icon: 'mdi-logout', onClick: this.onClickLogout },
+  ];
+
+  async onClickSettings() {
+    console.log('%c [JL] onClickSettings - settings', 'font-size: 13px; color:  orange;');
+  }
+
   async onClickLogout() {
     await this.$auth.logout();
     notifySuccess('Logged Out!');
