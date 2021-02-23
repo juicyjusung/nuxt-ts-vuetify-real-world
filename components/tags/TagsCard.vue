f7405e0bbd97df2301016ac9f21d241fca687b8e
juicyjusung <juicyjusung@gmail.com>
2021-02-24 08:58:14 +0900

    feat: add tags card component

---
+++ components/tags/TagsCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card>
+    <v-card-title>tags</v-card-title>
+    <v-card-text>
+      <v-chip v-for="(item, i) in tags" :key="i" class="ma-1" small>{{ item }}</v-chip>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator';
+import { Tag } from '~/models/tag/tag.types';
+
+@Component({
+  components: {},
+})
+export default class TagsCard extends Vue {
+  @Prop(Array) tags!: Tag[];
+}
+</script>
